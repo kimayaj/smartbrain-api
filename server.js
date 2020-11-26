1d099774aada07daec1e0d30ab0c7fd71110a999
Kimaya Joshi <kimayajoshi@Kimayas-MacBook-Air.local>
2020-11-27 03:01:12 +0530

    updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./Controllers/image');
 const db = knex ({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-round-72409',
     user : 'kimayajoshi',
     password : '',
     database : 'smart-brain'
