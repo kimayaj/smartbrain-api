0dfce3492e56abaa0d6156a10f2d994c463d1573
Kimaya Joshi <kimayajoshi@Kimayas-MacBook-Air.local>
2020-11-27 03:39:33 +0530

    fixing ssl issue

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const signin = require('./Controllers/signin');
 const profile = require('./Controllers/profile');
 const image = require('./Controllers/image');
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
+
 const db = knex ({
   client: 'pg',
   connection: {
