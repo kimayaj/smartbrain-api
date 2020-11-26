6d8efbe2f2539d42931736b7348e614b63809c66
Kimaya Joshi <kimayajoshi@Kimayas-MacBook-Air.local>
2020-11-27 02:01:17 +0530

    backend for facerecognition project

---
+++ Controllers/image.js
@@ -0,0 +1,32 @@
+const Clarifai = require ('clarifai');
+
+const app = new Clarifai.App({
+  apiKey: 'd2d6882397ca4d95869011b5215bc900'
+});
+
+const handleApiCall = (req, res) => {
+app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL, 
+        request.body.input)
+      .then( data => {
+      	res.json(data);
+      })
+      .catch (err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+	const { id } = req.body;
+	 db('users').where('id', '=', id)
+	 .increment('entries', 1)
+	 .returning('entries')
+	 .then(entries => {
+	  res.json(entries[0]);
+	 })
+	 .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+		handleImage: handleImage,
+		handleApiCall: handleApiCall
+	};
---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./Controllers/register');
+const signin = require('./Controllers/signin');
+const profile = require('./Controllers/profile');
+const image = require('./Controllers/image');
+
+const db = knex ({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'kimayajoshi',
+    password : '',
+    database : 'smart-brain'
+  }
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.get('/', (req, res)=>{
+	res.send('success');
+});
+
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
+
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
+
+app.get('/profile/:id', (req,res) => {profile.handleProfileGet(req, res, db)})
+
+app.put('/image', (req,res) => {image.handleImage(req, res, db)})
+
+
+app.listen(3000, () => {
+	console.log('running on 3000');
+});
+
