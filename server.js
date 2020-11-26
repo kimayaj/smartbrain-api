3121b4e3bc48197015ef0c259a838f486dd30396
Kimaya Joshi <kimayajoshi@Kimayas-MacBook-Air.local>
2020-11-27 02:35:14 +0530

    added port

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get('/profile/:id', (req,res) => {profile.handleProfileGet(req, res, db)})
 app.put('/image', (req,res) => {image.handleImage(req, res, db)})
 
 
-app.listen(3000, () => {
-	console.log('running on 3000');
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`running on port ${process.env.PORT}`);
 });
 
